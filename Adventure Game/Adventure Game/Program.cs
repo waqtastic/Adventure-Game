2f18f71ec9c9ab877061b19a32d00da4be618549
Waqtastic <waqar.m@outlook.com>
2019-07-29 11:13:47 +0100

    Initial commit

---
+++ Adventure Game/Adventure Game/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AdventureGame
+{
+	class Program
+	{
+		static void Main()
+		{
+			string CharacterName = "Pless Haldur";
+
+			Console.WriteLine("Welcome to Jetbuilders!");
+			Console.WriteLine("Our motto is: Create. Construct. Conclude.");
+			Console.WriteLine("Enter your name:");
+
+			CharacterName = Console.ReadLine();
+
+			Console.WriteLine(CharacterName + "? Well no one gets to choose their own name I guess. Welcome aboard!");
+		}
+	}
+}
