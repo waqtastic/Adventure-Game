9807f87d56a15a42e89f2ebe2583454ae27494ad
Waqtastic <waqar.m@outlook.com>
2019-07-31 11:57:18 +0100

    Colour changes and refactored!

--- Adventure Game/Adventure Game/Program.cs
+++ Adventure Game/Adventure Game/Program.cs
@@ -2,29 +2,70 @@
 
 namespace AdventureGame
 {
-	class Game
+	public static class Game
 	{
-		static void Main()
+		
+		static string CharacterName = "Pless Haldur";
+		
+		public static void StartGame()
 		{
+			Dialogue("Welcome to JETBUILDERS!" , "white");
+			Dialogue("Our motto is: CREATE. CONSTRUCT. CONCLUDE." , "cyan");
+			NameCharacter();
 		}
+
+		static void NameCharacter()
+		{
+			Console.WriteLine("Enter your name:");
+			CharacterName = Console.ReadLine();
+
+			Console.WriteLine("Welcome aboard " + CharacterName +  "! I'm sure you'll do just fine.");
+		}
+
+		static void Dialogue(string message, string color)
+		{
+			if (color== "red")
+			{ Console.ForegroundColor = ConsoleColor.Red; }
+			else if (color == "green")
+			{ Console.ForegroundColor = ConsoleColor.Green; }
+			else if (color == "yellow")
+			{ Console.ForegroundColor = ConsoleColor.Yellow; }
+			else if (color == "cyan")
+			{ Console.ForegroundColor = ConsoleColor.Cyan; }
+			else
+			{ Console.ForegroundColor = ConsoleColor.White; }
+
+			Console.WriteLine(message);
+			Console.ResetColor();
+		}
+
+		
 	}
+	
 	class Item
 	{
 
 	}
 	class Program
 	{
-
+		static void Main()
+		{
+			Game.StartGame();
+			Console.Read();
+		}
 	}
 
 }
 
-//string charactername = "pless haldur";
+/*string charactername = "pless haldur";
 
-//console.writeline("welcome to jetbuilders!");
-//			console.writeline("our motto is: create. construct. conclude.");
-//			console.writeline("enter your name:");
+console.writeline("welcome to jetbuilders!");
+			console.writeline("our motto is: create. construct. conclude.");
+			console.writeline("enter your name:");
 
-//			charactername = console.readline();
+			charactername = console.readline();
 
-//			console.writeline(charactername + "? well no one gets to choose their own name i guess. welcome aboard!");
+			console.writeline(charactername + "? well no one gets to choose their own name i guess. welcome aboard!");
+			*/
+
+	
